d8ae6a8d9f709dad48bda4007ca8826aacaa4173
Lontchi Frank <fodjof6@gmail.com>
2022-01-26 20:09:08 +0100

    done with unexpected

--- src/components/Unexpected.js
+++ src/components/Unexpected.js
@@ -1,23 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import styles from "../pages/NotFound/NotFound.module.css";
 
 function Unexpected() {
   const data = useSelector((state) => state.admin);
   const { error } = data;
   return (
-    <div className="mt-5">
-      <div>
-        <span className="display-9 ">Oops something went Wrong !!</span>
-      </div>
-
-      <img
-        src="/unexpected.png"
-        alt="Something went wrong"
-        width="70%"
-        height="400"
-      />
-      <div>
-        <span className="display-6 text-danger ">{error}!!</span>
+    <div
+      className={`${styles.notfound} d-flex flex-column justify-content-center align-items-center`}
+    >
+      <h3 style={{ color: "#7B7B7B" }} className="pt-3">
+        {error}!!!
+      </h3>
+      <div className={`${styles.notimage}`}>
+        <img src="/not.png" alt="" style={{ width: "80%", height: "90%" }} />
       </div>
     </div>
   );
