ea7c08215cfb250218aad82b875552245045726f
LontchiFrank <fodjof6@gmail.com>
2022-03-15 10:55:12 +0100

    Thank God

--- src/pages/Feedback/Feedback.jsx
+++ src/pages/Feedback/Feedback.jsx
@@ -7,7 +7,7 @@ function Feedback() {
         <div class="wrapper wrapper--w790">
           <div class="card card-5">
             <div class="card-heading">
-              <h2 class="title">Event Registration Form</h2>
+              <h2 class="title">Weekly Feedback Form</h2>
             </div>
             <div class="card-body">
               <form method="POST">
@@ -22,7 +22,7 @@ function Feedback() {
                             type="text"
                             name="first_name"
                           />
-                          <label class="label--desc">first name</label>
+                          <label class="label--desc">Name</label>
                         </div>
                       </div>
                       <div class="col-2">
@@ -32,24 +32,13 @@ function Feedback() {
                             type="text"
                             name="last_name"
                           />
-                          <label class="label--desc">last name</label>
+                          <label class="label--desc">Sponsor's name</label>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class="form-row">
-                  <div class="name">Company</div>
-                  <div class="value">
-                    <div class="input-group">
-                      <input
-                        class="input--style-5"
-                        type="text"
-                        name="company"
-                      />
-                    </div>
-                  </div>
-                </div>
+
                 <div class="form-row">
                   <div class="name">Email</div>
                   <div class="value">
@@ -85,44 +74,24 @@ function Feedback() {
                     </div>
                   </div>
                 </div>
-                <div class="form-row">
-                  <div class="name">Subject</div>
-                  <div class="value">
-                    <div class="input-group">
-                      <div class="rs-select2 js-select-simple select--no-search">
-                        <select name="subject">
-                          <option disabled="disabled" selected="selected">
-                            Choose option
-                          </option>
-                          <option>Subject 1</option>
-                          <option>Subject 2</option>
-                          <option>Subject 3</option>
-                        </select>
-                        <div class="select-dropdown"></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+
                 <div class="form-row p-t-20">
-                  <label class="label label--block">
-                    Are you an existing customer?
-                  </label>
-                  <div class="p-t-15">
-                    <label class="radio-container m-r-55">
-                      Yes
-                      <input type="radio" checked="checked" name="exist" />
-                      <span class="checkmark"></span>
-                    </label>
-                    <label class="radio-container">
-                      No
-                      <input type="radio" name="exist" />
-                      <span class="checkmark"></span>
-                    </label>
+                  <label class="label label--block">Upload Task</label>
+                  <div class="p-t-15 ">
+                    <div className="col-md-12">
+                      <input
+                        required
+                        type="file"
+                        name="upload"
+                        accept="application/pdf , application/msword,application/vnd.ms-excel,application/vnd.ms-powerpoint,text/plain"
+                        placeholder="Select A Book"
+                      />
+                    </div>
                   </div>
                 </div>
                 <div>
                   <button class="btn btn--radius-2 btn--red" type="submit">
-                    Register
+                    Submit Task
                   </button>
                 </div>
               </form>
